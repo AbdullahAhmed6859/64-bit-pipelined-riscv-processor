8e645528a8cf65c72b265b15f1ae892785391bff
AbdullahAhmed6859 <abdullahahmed2004@gmail.com>
2024-09-29 11:53:25 +0500

    Lab 6 update
    Added A low level implementation of a 64 Bit ALU, made few updates.

---
+++ 32bit-riscv-processor.srcs/Lab-6/sources/alu_64bit_low_level.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module alu_64bit_low_level(
+    input [63:0] a,
+    input [63:0] b,
+    input [3:0] alu_op,
+    output zero,
+    output [63:0] result
+    );
+
+    wire [63:0] carry;
+    wire carry_in = (alu_op == 4'b0110) ? 1'b1 : 1'b0;
+    assign zero = ~(|result);
+    // no carry in
+    alu_1bit alu0 (a[0], b[0], alu_op, carry_in , carry[0], result[0]);
+
+    genvar i;
+    generate
+        for (i = 1; i <= 62; i = i + 1) begin : alu_loop
+            alu_1bit alu (a[i], b[i], alu_op, carry[i-1], carry[i], result[i]);
+        end
+    endgenerate
+    
+    // no carry out
+    alu_1bit alu63 (a[63], b[63], alu_op, carry[62], , result[63]);
+
+endmodule
--- 32bit-riscv-processor.srcs/Lab-6/sources/alu_8bit.v
+++ 32bit-riscv-processor.srcs/Lab-6/sources/alu_8bit.v
@@ -8,17 +8,16 @@ module alu_8bit(
     output carry_out,
     output [7:0] result
     );
+    wire [0:6]  carry_outi;
     
-    wire carry_out1, carry_out2, carry_out3, carry_out4, carry_out5, carry_out6, carry_out7;
-    
-    alu_1bit alu0 (a[0], b[0], alu_op, carry_in, carry_out1, result[0]);
-    alu_1bit alu1 (a[1], b[1], alu_op, carry_out1, carry_out2, result[1]);
-    alu_1bit alu2 (a[2], b[2], alu_op, carry_out2, carry_out3, result[2]);
-    alu_1bit alu3 (a[3], b[3], alu_op, carry_out3, carry_out4, result[3]);
-    alu_1bit alu4 (a[4], b[4], alu_op, carry_out4, carry_out5, result[4]);
-    alu_1bit alu5 (a[5], b[5], alu_op, carry_out5, carry_out6, result[5]);
-    alu_1bit alu6 (a[6], b[6], alu_op, carry_out6, carry_out7, result[6]);
-    alu_1bit alu7 (a[7], b[7], alu_op, carry_out7, carry_out, result[7]);
+    alu_1bit alu0 (a[0], b[0], alu_op, carry_in, carry_outi[0], result[0]);
+    alu_1bit alu1 (a[1], b[1], alu_op, carry_outi[0], carry_outi[1], result[1]);
+    alu_1bit alu2 (a[2], b[2], alu_op, carry_outi[1], carry_outi[2], result[2]);
+    alu_1bit alu3 (a[3], b[3], alu_op, carry_outi[2], carry_outi[3], result[3]);
+    alu_1bit alu4 (a[4], b[4], alu_op, carry_outi[3], carry_outi[4], result[4]);
+    alu_1bit alu5 (a[5], b[5], alu_op, carry_outi[4], carry_outi[5], result[5]);
+    alu_1bit alu6 (a[6], b[6], alu_op, carry_outi[5], carry_outi[6], result[6]);
+    alu_1bit alu7 (a[7], b[7], alu_op, carry_outi[6], carry_out, result[7]);
 
     
 endmodule
